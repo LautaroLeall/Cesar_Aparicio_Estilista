1566a1fa40021c79d8f78cd796ae49a7c99c173f
LautaroLeall <lautaroleal4@gmail.com>
2026-03-13 16:12:21 -0300

    update main.jsx (call global.cssa and add animations aos)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import './styles/global.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export function Root() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+      offset: 120,
+    })
+  }, [])
+
+  return (
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
+}
+
+createRoot(document.getElementById('root')).render(<Root />)
